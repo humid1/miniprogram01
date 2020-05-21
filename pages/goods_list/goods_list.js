3ce04418888b5fff01a5648c53249a2b44331ca6
qjp <qiujianp123@163.com>
2020-05-21 18:18:56 +0800

    商品列表-筛选的效果、商品的显示

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -1,3 +1,5 @@
+import { request } from "../../request/index";
+
 // pages/goods_list/goods_list.js
 Page({
 
@@ -5,16 +7,61 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        value: '综合',
+        isActive: true
+      },
+      {
+        id: 1,
+        value: '销量',
+        isActive: false
+      },
+      {
+        id: 2,
+        value: '价格',
+        isActive: false
+      }
+    ],
+    // 商品列表数据
+    goodsList: []
   },
-
+  // 获取商品列表的接口需要提供的参数
+  QueryParams: {
+    query: '',
+    cid: '',
+    pagenum: 1,
+    pagesize: 10
+  },
+  // 修改 isActive 状态
+  handleItemChange(e) {
+    // console.log(e);
+    // 获取标签索引
+    const {index} = e.detail;
+    // 修改源数据
+    let {tabs} = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive=true : v.isActive=false)
+    this.setData({
+      tabs
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.QueryParams.cid = options.cid;
+    this.getGoodsList();
   },
-
+  // 获取商品列表数据
+  getGoodsList() {
+    request({url: '/goods/search', data: this.QueryParams}).then( result => {
+      const goodsList = result.data.message.goods
+      this.setData({
+        goodsList
+      })
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -54,13 +101,6 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
