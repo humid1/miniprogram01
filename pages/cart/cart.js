b016c89d6a20cdde97bc90985096dbf866c28071
qjp <qiujianp123@163.com>
2020-05-23 18:09:03 +0800

    商品加入购物车功能，购物车页面渲染

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,3 +1,28 @@
+/**
+ * 1.获取用户收货地址
+ *    绑定点击事件
+ *    调用小程序内置 api 获取用户收货地址 wx.chooseAddress()
+ * 2.获取用户对小程序授权 获取地址 权限状态
+ *    wx.getSetting()
+ *    假设用户 点击获取收货地址的提示框 确定(可直接调用获取收货地址)
+ *      scope.address 值 true
+ *    假设用户 从未调用过 收货地址的 api  (可直接调用获取收货地址)  
+ *      scope undefined
+ *    假设用户 点击获取收货地址的提示框 取消
+ *      scope.address 值 false
+ *    打开设置界面，引导用户开启获取收货地址授权 wx.openSetting()
+ * 3.页面加载完毕
+ *    onShow()
+ *    获取本地存储的地址数据
+ *    把数据设置给 data 中的一个变量
+ * 4.获取缓存中的商品数据
+ *    商品详情页，添加购物车所需的额外值 num checked
+ * 5.全选的实现，数据展示
+ *    onShow 获取缓存中的购物车数据
+ *    根据购物车中的商品数据，所有商品都被选中 checked=true   
+ */
+import { getSetting,openSetting,chooseAddress } from '../../utils/asyncWx';
+import regeneratorRuntime from '../../lib/runtime/runtime';
 // pages/cart/cart.js
 Page({
 
@@ -5,7 +30,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    address: {},
+    cart: [],
+    allChecked: false
   },
 
   /**
@@ -14,53 +41,111 @@ Page({
   onLoad: function (options) {
 
   },
-
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 获取微信缓存中的数据（生产环境是获取后台数据） 
    */
-  onReady: function () {
-
+  onShow() {
+    // 1.获取缓存中的收货地址信息
+    const address = wx.getStorageSync('address');
+    // 2.获取缓存中的商品数据
+    const cart = wx.getStorageSync('cart') || [];
+    // 3.计算全选
+    // every 数组方法，会遍历，会接收一个回调函数，那么每一个回调函数都返回 true，那么every 方法的返回值为 true
+    // 只要有一个回调函数返回了 false，那么就不会在循环执行，直接返回 false
+    const allChecked = cart.length ? cart.every(v => v.checked) : false;
+    // 给data赋值
+    this.setData({
+      address,
+      cart,
+      allChecked
+    })
   },
-
   /**
-   * 生命周期函数--监听页面显示
+   * 点击 获取收货地址 按钮
    */
-  onShow: function () {
+  async handleChooseAddress(e) {
+    // 1.获取 权限状态
+    // wx.getSetting({
+    //   success: (result)=>{
+    //     // console.log(result)
+    //     const scopeAddress = result.authSetting['scope.address'];
+    //     if(scopeAddress === true || scopeAddress === undefined) {
+    //       // 获取微信收货地址
+    //       wx.chooseAddress({
+    //         success: (result2)=>{
+    //           console.log(result2)
+    //         },
+    //         fail: (err)=>{
+    //           wx.showToast({
+    //             title: '获取收货地址失败',
+    //             icon: 'none'
+    //           });
+    //         }
+    //       });
+    //     } else {
+    //       // 用户以前拒绝授权，引导用户打开权限页面
+    //       wx.openSetting({
+    //         success: (result2)=>{
+    //           // 调用获取 收货地址代码
+    //           wx.chooseAddress({
+    //             success: (result2)=>{
+    //               console.log(result2)
+    //             },
+    //             fail: (err)=>{
+    //               wx.showToast({
+    //                 title: '获取收货地址失败',
+    //                 icon: 'none'
+    //               });
+    //             }
+    //           });
+    //         }
+    //       });
+    //     }
+    //   }
+    // });
 
-  },
+    // 封装为 Promise 语法
+    // getSetting().then((res) => {
+    //   const scopeAddress = res.authSetting['scope.address'];
+    //   if(scopeAddress === true || scopeAddress === undefined) {
+    //     return true;
+    //   } else {
+    //     return openSetting();
+    //   }
+    // }).then( res2 => {
+    //   return chooseAddress();
+    // }).then( res3 => {
+    //   console.log(res3);
+    // }, err => {
+    //   wx.showToast({
+    //     title: '获取收货地址失败',
+    //     icon: 'none'
+    //   });
+    // })
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    // async-await语法 （需在方法前加上 async）
+    // 1.获取 权限状态
+    try{
+      const res = await getSetting();
+      // 2.判断权限状态
+      if(res.authSetting['scope.address'] === false) {
+        // 用户以前拒绝授权，引导用户打开权限页面
+        await openSetting();
+      }
+      // 3.获取收货地址信息
+      let address = await chooseAddress().catch(err => {
+        wx.showToast({
+          title: '获取收货地址失败',
+          icon: 'none'
+        });
+      });
+      // 4.存入到缓存中
+      // console.log(address);
+      address.all = address.provinceName + address.cityName + address.countyName + address.detailInfo;
+      wx.setStorageSync('address', address)
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    } catch(err) {
+      console.log(err);
+    }
   }
 })
--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -1,3 +1,13 @@
+/**
+ * 1.发送请求获取数据
+ * 2.点击轮播图，预览大图
+ * 3.点击加入购物车
+ *  先绑定点击事件
+ *  获取缓存中的购物车数据-数组格式
+ *  先判断，当前商品是否已经存在购物车
+ *    存在，修改商品数据，执行购物车商品数量 ++，重新把购物车数据，填充到缓存中
+ *    不存在购物车，直接给购物车添加一个新元素
+ */
 import { request } from "../../request/index";
 
 // pages/goods_detail/goods_detail.js
@@ -10,7 +20,7 @@ Page({
     goodsObj: {}
   },
   // 构造商品对象
-  GoodObj: {},
+  GoodInfo: {},
   /**
    * 生命周期函数--监听页面加载
    */
@@ -22,7 +32,7 @@ Page({
   getGoodsDetail(goods_id) {
     request({ url:"/goods/detail", data: {goods_id} }).then( res => {
       const goodsObj = res.data.message;
-      this.GoodObj = res.data.message;
+      this.GoodInfo = res.data.message;
       this.setData({
         goodsObj: {
           goods_name: goodsObj.goods_name,
@@ -40,13 +50,38 @@ Page({
   // 点击轮播图 放大预览 
   handlePreviewImage(e) {
     // 先构造要预览的图片数组
-    const urls = this.GoodObj.pics.map( v => v.pics_mid);
+    const urls = this.GoodInfo.pics.map( v => v.pics_mid);
     // 接收传递过来的图片URL
     const current = e.currentTarget.dataset.url;
     wx.previewImage({
       current,
       urls
     });
+  },
+  // 点击 加入购物车
+  handleCartAdd(e) {
+    // 1.获取缓存中的购物车数据
+    let cart = wx.getStorageSync('cart') || [];
+    // 2.判断商品对象是否存在于购物车数组中
+    let index = cart.findIndex(v => v.goods_id === this.GoodInfo.goods_id);
+    if(index === -1) {
+      // 第一次添加
+      this.GoodInfo.num = 1;
+      this.GoodInfo.checked = true
+      cart.push(this.GoodInfo)
+    } else {
+      // 存在该商品数据
+      cart[index].num++;
+    }
+    // 3.把购物车重新添加到缓存中
+    wx.setStorageSync('cart', cart);
+    // 4.弹框提示
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      // 添加遮罩层，防止频繁点击
+      mask: true
+    })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
